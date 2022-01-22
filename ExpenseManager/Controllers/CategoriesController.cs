9935733b14f77133cec4844581d57e7c5e0f9131
Mahipal <Mahirao5464@gmail.com>
2022-01-22 12:06:37 +0530

    Add project files.

---
+++ ExpenseManager/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using ExpenseManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManager.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            Category[] categories =
+            {
+                new Category{ 
+                Id = 1,
+                Title = "Clothing",
+                Description = "Test Desc",
+                CategoryType = CategoryType.Expense,
+                CreatedOn = DateTime.Now,
+                UpdatedOn = DateTime.Now
+                },
+                new Category{
+                Id = 2,
+                Title = "Salary",
+                Description = "Test Desc",
+                CategoryType = CategoryType.Income,
+                CreatedOn = DateTime.Now,
+                UpdatedOn = DateTime.Now
+                },
+                new Category{
+                Id = 3,
+                Title = "Food",
+                Description = "Test Desc",
+                CategoryType = CategoryType.Expense,
+                CreatedOn = DateTime.Now,
+                UpdatedOn = DateTime.Now
+                },
+                new Category{
+                Id = 4,
+                Title = "Traveling",
+                Description = "Test Desc",
+                CategoryType = CategoryType.Expense,
+                CreatedOn = DateTime.Now,
+                UpdatedOn = DateTime.Now
+                }
+            };  // Categories will be fetched from database in realtime 
+
+
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExpenseManager/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace ExpenseManager.Models
+{
+    public class Category
+    {
+        public int Id;
+        public string Title;
+        public string Description;
+        public CategoryType CategoryType;
+        public DateTime CreatedOn;
+        public DateTime UpdatedOn;
+    }
+    public enum CategoryType
+    {
+        Income,
+        Expense
+    }
+}
